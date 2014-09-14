f6d55075b0ac8b66c2ca63df6040bdf387c5acda
Ferhat Yeter <41320@longroad.ac.uk>
2014-09-14 23:35:04 +0100

    homework task 6.1

---
+++ Homework task 6.1.py
@@ -0,0 +1,11 @@
+#Ferhat Yeter
+#14/09/14
+#Homework task 6.1
+
+number0 = float(input("Please enter the first number you would like to add up: "))
+number1 = float(input("Please enter the second number you would like to add up: "))
+number2 = float(input("Please enter the third and final number you would like to add up: "))
+
+number3 = number0 + number1 + number2
+
+print("The sum of these numbers is: {0}".format(number3))
