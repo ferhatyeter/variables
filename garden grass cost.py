4764154a00d32d94eb84845c2959d622f986d709
Ferhat Yeter <41320@longroad.ac.uk>
2014-09-16 11:13:53 +0100

    .

---
+++ garden grass cost.py
@@ -0,0 +1,16 @@
+#Ferhat Yeter
+#16-09-14
+#Exercise garden grass cost
+
+garden_length = float(input("Please enter the length of your garden in meters: "))
+garden_width = float(input("Please enter the width of your garden in meters: "))
+border = float(input("Please enter the border aroud the lawn you dont want to turf: "))
+
+new_garden_length = garden_length - border
+new_garden_width = garden_width - border
+
+area = new_garded_length * new_garden_width
+
+cost = area * 10
+
+print("the cost of new turf is: {0:.0f}.".format(cost))
